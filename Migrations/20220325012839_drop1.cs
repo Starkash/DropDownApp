092f5f237fbb1945ec6a2517c013f4d5c96f88df
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-25 07:02:18 +0530

    Add project files.

---
+++ Migrations/20220325012839_drop1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DropDownApp.Migrations
+{
+    public partial class drop1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
